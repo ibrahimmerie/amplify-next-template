d185507d2f62125fbeb210efb9ffb4a465f47525
Ibrahim Merie <ibrahim.merie.fl@gmail.com>
2025-04-20 13:40:16 -0400

    users can only access their own data

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
 
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
@@ -40,6 +40,8 @@ export default function App() {
 
   return (
     <main>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
